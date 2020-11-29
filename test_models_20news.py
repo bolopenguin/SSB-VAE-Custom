3e294f27339865bec49656b55b16d33eededbe35
bolopenguin <damianobolognini1997@gmail.com>
2020-11-29 16:29:41 +0100

    provashape

--- test_models_20news.py
+++ test_models_20news.py
@@ -23,7 +23,7 @@ def run_20_news(model_id,percentage_supervision,nbits_for_hashing,alpha_val,gamm
 
 	filename = 'Data/ng20.tfidf.mat'
 	data = Load_Dataset(filename)
-	print(data.head())
+	print(data)
 	X_train_input = data["train"]
 	X_train = X_train_input
 	X_train = X_train
